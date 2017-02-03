3115137f4b5599bbeb49416caeeb16d7bab8d92f
Roland Kofler <roland.kofler@gmail.com>
2017-02-03 15:03:10 +0100

    always commit twice ;-)

--- Charles.sol
+++ Charles.sol
@@ -2,10 +2,10 @@ pragma solidity ^0.4.6;
 
 contract ACharlesTester{
     
-    Charles public c;
+    Charles public c = new Charles();
     
     function tst(){
-        var c = new Charles();
+        
         c.commitTo("don't drink", this, 1 days , 14);
         c.witnessTo(this, 1, true);
         c.payout();
@@ -25,7 +25,8 @@ contract Charles{
         uint32 frequency;
         uint amount;
         bool[] witnessed;
-        bool[] topay;
+        bool[] toPayBack;
+        bool[] toGiveToAnticharity;
     }
     
     mapping (address => Commitment) public commited;
@@ -34,9 +35,9 @@ contract Charles{
     function commitTo(string _cause, address _witness, uint32 _period, uint32 _frequency) external payable
     {
         // check if commitment needs to be made if () throw;
-        bool[] w;
-        bool[] p;
-        var c =Commitment(msg.sender, _cause, _witness, _period, _frequency, msg.value, w, p);
+        bool[] memory w;
+        
+        var c =Commitment(msg.sender, _cause, _witness, _period, _frequency, msg.value, w, w, w);
         commited[msg.sender]= c;
         
         NewCommitmentCreated(msg.sender, _cause, _witness, _period, _frequency);
@@ -48,7 +49,8 @@ contract Charles{
         var c = commited[_commiter];
         if (c.witness != msg.sender) throw; 
         c.witnessed[_period] = _isOk;
-        c.topay[_period] = _isOk;
+        c.toPayBack[_period] = _isOk;
+        c.toGiveToAnticharity[_period] = !_isOk;
         CommitmentWitnessed(c.committer, c.cause, c.witness, _period, _isOk);
     }
     event CommitmentWitnessed(address committer, string cause, address witness, uint period, bool isOk);
@@ -57,11 +59,28 @@ contract Charles{
         var c = commited[msg.sender];
         uint count =0;
         for(uint i = 0; i < c.frequency; i++){
-            if (c.topay[i]) count ++;
-            c.topay[i] = false;
+            if (c.toPayBack[i]) count ++;
+            c.toPayBack[i] = false;
             
         }
-        msg.sender.send(c.amount*count/c.frequency); //TODO: solve binary division rounding problem
+        var amount = c.amount*count/c.frequency;
+        bool success = msg.sender.send(amount); //TODO: solve binary division rounding problem
+        PayedBack(msg.sender, amount, c.cause);
     }
+    event PayedBack(address _commiter, uint amount, string cause);
+    
+    function payAnticharity(){
+        var c = commited[msg.sender];
+        uint count =0;
+        for(uint i = 0; i < c.frequency; i++){
+            if (c.toGiveToAnticharity[i]) count ++;
+            c.toGiveToAnticharity[i] = false;
+            
+        }
+        var amount = c.amount*count/c.frequency;
+        bool success = msg.sender.send(amount); //TODO: solve binary division rounding problem
+        PayedBack(msg.sender, amount, c.cause);
+    }
+    event PayedBack(address _commiter, uint amount, string cause);
     
 }
