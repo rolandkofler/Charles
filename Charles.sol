c61a109bb3bbdc0625a522adb887ff872434f6f6
Roland Kofler <roland.kofler@gmail.com>
2017-02-03 14:52:29 +0100

    the ground concept. needs heavy testing

--- Charles.sol
+++ Charles.sol
@@ -4,9 +4,11 @@ contract ACharlesTester{
     
     Charles public c;
     
-    function ACharlesTester(){
+    function tst(){
         var c = new Charles();
-        c.commitTo("don't drink", 0xaffe, 1 days , 14 days);
+        c.commitTo("don't drink", this, 1 days , 14);
+        c.witnessTo(this, 1, true);
+        c.payout();
     }
 }
 
@@ -16,25 +18,50 @@ contract Charles{
     address constant public theDaoHacker= 0xF35e2cC8E6523d683eD44870f5B7cC785051a77D; // according to Köppelmann here http://hackingdistributed.com/2016/06/18/analysis-of-the-dao-exploit/
     
     struct Commitment{
+        address committer;
         string cause; 
         address witness; 
         uint32 period; 
         uint32 frequency;
+        uint amount;
+        bool[] witnessed;
+        bool[] topay;
     }
     
-    mapping (address => Commitment) commitments;
+    mapping (address => Commitment) public commited;
+    
     
     function commitTo(string _cause, address _witness, uint32 _period, uint32 _frequency) external payable
     {
         // check if commitment needs to be made if () throw;
-        commitments[msg.sender]= Commitment(_cause, _witness, _period, _frequency);
-        NewCommitment(msg.sender, _cause, _witness, _period, _frequency);
-    }
-    event NewCommitment(address committer, string cause, address witness, uint32 period, uint32 frequency);
-    
-    function witness() external
-    {
+        bool[] w;
+        bool[] p;
+        var c =Commitment(msg.sender, _cause, _witness, _period, _frequency, msg.value, w, p);
+        commited[msg.sender]= c;
         
+        NewCommitmentCreated(msg.sender, _cause, _witness, _period, _frequency);
+    }
+    event NewCommitmentCreated(address committer, string cause, address witness, uint period, uint frequency);
+    
+    function witnessTo(address _commiter, uint _period, bool _isOk) external 
+    {
+        var c = commited[_commiter];
+        if (c.witness != msg.sender) throw; 
+        c.witnessed[_period] = _isOk;
+        c.topay[_period] = _isOk;
+        CommitmentWitnessed(c.committer, c.cause, c.witness, _period, _isOk);
+    }
+    event CommitmentWitnessed(address committer, string cause, address witness, uint period, bool isOk);
+    
+    function payout(){
+        var c = commited[msg.sender];
+        uint count =0;
+        for(uint i = 0; i < c.frequency; i++){
+            if (c.topay[i]) count ++;
+            c.topay[i] = false;
+            
+        }
+        msg.sender.send(c.amount*count/c.frequency); //TODO: solve binary division rounding problem
     }
     
 }
